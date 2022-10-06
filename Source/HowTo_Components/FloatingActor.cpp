4c1701e04f40e57a6dd2dbb9607188004af45be6
Gregory Travis <greg.m.travis@gmail.com>
2022-10-05 22:18:59 -0400

    rotation sorta working

--- Source/HowTo_Components/FloatingActor.cpp
+++ Source/HowTo_Components/FloatingActor.cpp
@@ -60,11 +60,17 @@ FVector fvector_to_FVector(sexp fvector)
   sexp x = ke_get_field(fvector, "x");
   sexp y = ke_get_field(fvector, "y");
   sexp z = ke_get_field(fvector, "z");
-  FVector fv = {
+  /*
+   FVector fv = {
     (float)SEXP_GET_INTEGER(x),
     (float)SEXP_GET_INTEGER(y),
     (float)SEXP_GET_INTEGER(z)
   };
+   */
+    FVector fv;
+    fv.X = (float)SEXP_GET_INTEGER(x);
+    fv.Y = (float)SEXP_GET_INTEGER(y);
+    fv.Z = (float)SEXP_GET_INTEGER(z);
   return fv;
 }
 
@@ -73,11 +79,15 @@ FRotator frotator_to_FRotator(sexp frotator)
   sexp pitch = ke_get_field(frotator, "pitch");
   sexp roll = ke_get_field(frotator, "roll");
   sexp yaw = ke_get_field(frotator, "yaw");
-  FRotator fr = {
+  /*FRotator fr = {
     (float)SEXP_GET_INTEGER(pitch),
     (float)SEXP_GET_INTEGER(roll),
     (float)SEXP_GET_INTEGER(yaw)
-  };
+  };*/
+    FRotator fr;
+    fr.Pitch = (float)SEXP_GET_INTEGER(pitch);
+    fr.Roll = (float)SEXP_GET_INTEGER(roll);
+    fr.Yaw = (float)SEXP_GET_INTEGER(yaw);
   return fr;
 }
 
@@ -88,7 +98,7 @@ sexp GetActorRotation_delegate_sexp_native(sexp arglist)
   sexp kactor_sexp = car(arglist);
   AFloatingActor *kactor = (AFloatingActor*)SEXP_GET_OBJ(kactor_sexp);
   FRotator rotation = kactor->GetActorRotation();
-  printf("Returning frotator\n");
+  //printf("Returning frotator\n");
   return FRotator_to_frotator(kactor, rotation);
 }
 
@@ -99,7 +109,7 @@ sexp GetActorLocation_delegate_sexp_native(sexp arglist)
   sexp kactor_sexp = car(arglist);
   AFloatingActor *kactor = (AFloatingActor*)SEXP_GET_OBJ(kactor_sexp);
   FVector location = kactor->GetActorLocation();
-  printf("Returning fvector\n");
+  //printf("Returning fvector\n");
   return FVector_to_fvector(kactor, location);
 }
 
@@ -110,10 +120,10 @@ sexp SetActorLocationAndRotation_delegate_sexp_native(sexp arglist)
   sexp kactor_sexp = car(arglist);
   sexp location = cadr(arglist);
   sexp rotation = caddr(arglist);
-  printf("OOOO %d %d\n", SEXP_TYPE(location), SEXP_TYPE(rotation));
   AFloatingActor *kactor = (AFloatingActor*)SEXP_GET_OBJ(kactor_sexp);
   FVector fv = fvector_to_FVector(location);
   FRotator fr = frotator_to_FRotator(rotation);
+    UE_LOG(LogTemp, Warning, TEXT("AAA SALAR %f %f %f %f %f %f"), fv.X, fv.Y, fv.Z, fr.Pitch, fr.Roll, fr.Yaw);
   return kactor->SetActorLocationAndRotation(fv, fr);
 }
 
@@ -138,7 +148,6 @@ AFloatingActor::AFloatingActor()
         VisualMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
     }
 
-    /*
     ke_init();
 
     sexp super_class = ke_exec_file("/Users/gmt/proj/kinterp/super.k");
@@ -162,6 +171,7 @@ AFloatingActor::AFloatingActor()
     kdelegate = ke_call_constructor(clas, L1(super));
     fvector_class = ke_exec_file("/Users/gmt/proj/kinterp/fvector.k");
     frotator_class = ke_exec_file("/Users/gmt/proj/kinterp/frotator.k");
+    /*
      */
 }
 
@@ -177,9 +187,9 @@ void AFloatingActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-    //NuTick(DeltaTime);
+    NuTick(DeltaTime);
 
-//#if 0
+#if 0
     FVector NewLocation = GetActorLocation();
     FRotator NewRotation = GetActorRotation();
     UE_LOG(LogTemp, Warning, TEXT("BBBB0 %f %f %f %f %f %f"), NewLocation.X, NewLocation.Y, NewLocation.Z, NewRotation.Pitch, NewRotation.Roll, NewRotation.Yaw);
@@ -280,6 +290,6 @@ void AFloatingActor::Tick(float DeltaTime)
         }
     }
     //UE_LOG(LogTemp, Warning, TEXT("Some warning message") );
-//#endif
+#endif
 }
 
